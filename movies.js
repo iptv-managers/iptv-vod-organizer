01785a13cc100d95502515d88dc89c01d0936d55
Cleiton Costa <cleitoncosta83@gmail.com>
2025-08-21 18:06:51 -0300

    fix: ajustes

--- movies.js
+++ movies.js
@@ -17,15 +17,12 @@
 import axios from "axios";
 import fs from "fs";
 import pLimit from "p-limit";
-import dotenv from "dotenv";
 import { cleanName } from "./utils/clean-name.js";
 import { isSubtitled } from "./validations/is-subtitled.js";
 import { isAdult } from "./validations/is-adult.js";
 import { db } from "./utils/database.js";
 import { getGenreIds } from "./validations/get-genre.js";
 import { getYears } from "./validations/get-years.js";
-
-dotenv.config();
 
 const {
     TMDB_API_KEY, OUTPUT_FILE, TMDB_LANGUAGE,
@@ -37,6 +34,7 @@ const CONCURRENCY_LIMIT = 30; // Limite de requisições concorrentes
 // Aqui a gente salva as categorias existentes para consultar, caso 
 // não encontre através dos códigos
 let existingCategories = [];
+const limit = pLimit(CONCURRENCY_LIMIT);
 
 export async function createTMDB() {
     const [rows] = await db.query(
@@ -55,78 +53,7 @@ export async function createTMDB() {
     );
     existingCategories = cats;
 
-    const limit = pLimit(CONCURRENCY_LIMIT);
     const allResults = [];
-
-    async function fetchFromTMDB(stream) {
-        try {
-            let tmdbData;
-
-            // 1 - Se já tem TMDB ID, busca direto
-            if (stream.tmdb_id) {
-                const { data } = await axios.get(
-                    `https://api.themoviedb.org/3/movie/${stream.tmdb_id}`,
-                    { params: { api_key: TMDB_API_KEY, language: TMDB_LANGUAGE } }
-                );
-                tmdbData = data;
-            }
-
-            // 2 - Se não encontrou pelo ID, busca por nome + ano
-            if (!tmdbData) {
-                const query = stream.stream_display_name;
-                const { data } = await axios.get(
-                    `https://api.themoviedb.org/3/search/movie`,
-                    {
-                    params: {
-                        api_key: TMDB_API_KEY,
-                        query: cleanName(query),
-                        year: stream.year || undefined,
-                        language: TMDB_LANGUAGE,
-                    },
-                    }
-                );
-
-                if (data.results && data.results.length > 0) {
-                    tmdbData = data.results[0];
-                }
-            }
-
-            if (!tmdbData) {
-                console.warn(
-                    `⚠️ Não foi possível encontrar dados para: ${stream.stream_display_name} (${stream.tmdb_id || "sem id"})`
-                );
-            }
-            const existingCategoryNames = (JSON.parse(stream.category_id || "[]"))
-                .map((catId) => {
-                    const cat = existingCategories.find((c) => c.id === catId);
-                    return cat ? cat.category_name : null;
-                })
-                .filter(Boolean)
-                .join(", ");
-            
-            return {
-                ...{category_names: existingCategoryNames},
-                ...stream,
-                tmdb: tmdbData,
-            };
-        } catch (err) {
-            if(err?.message?.toString().includes('401')) {
-                console.error("❌ ============================================================= ❌ ");
-                console.error("Erro de autenticação com a TMDB. Verifique sua chave API.");
-                console.error("❌ ============================================================= ❌ ");
-                process.exit(1);
-            }
-            console.error(
-                `Erro TMDB: ${stream.stream_display_name} (${stream.tmdb_id || "sem id"})`,
-                err.message
-            );
-
-            return {
-                ...stream,
-                tmdb: null,
-            };
-        }
-    }
 
     // Processa em chunks
     for (let i = 0; i < (rows).length; i += CHUNK_SIZE) {
@@ -134,7 +61,7 @@ export async function createTMDB() {
         console.log(`Processando chunk ${Math.floor(i / CHUNK_SIZE) + 1} (${chunk.length} filmes)`);
 
         const processedChunk = await Promise.all(
-            chunk.map((stream) => limit(() => fetchFromTMDB(stream)))
+            chunk.map((stream) => limit(() => fetchFromTMDB(stream, existingCategories)))
         );
 
         allResults.push(...processedChunk);
@@ -144,37 +71,106 @@ export async function createTMDB() {
     // Salva todos os resultados em um único arquivo JSON
     fs.writeFileSync(OUTPUT_FILE, JSON.stringify(allResults, null, 2), 'utf-8');
     console.log(`🎉 Todos os filmes processados! JSON final salvo em: ${OUTPUT_FILE}`);
-    await organizeMovies();
+
+
+    const streams = JSON.parse(fs.readFileSync(OUTPUT_FILE, 'utf-8'));
+    const categories = JSON.parse(fs.readFileSync('./cat-movies.json', 'utf-8'));
+    await organizeMovies(streams, categories, true);
+
     console.log(`🎉 Organização de filmes concluída!`);
     process.exit(1);
 }
 
-export async function organizeMovies() {
-    if (!fs.existsSync(OUTPUT_FILE)) {
-        console.log(`O arquivo não existe.`);
-        fs.unlinkSync(OUTPUT_FILE);
-    }
+async function fetchFromTMDB(stream, existingCategories) {
+    try {
+        let tmdbData;
 
-    const streams = JSON.parse(fs.readFileSync(OUTPUT_FILE, 'utf-8'));
-    const categories = JSON.parse(fs.readFileSync('./cat-movies.json', 'utf-8'));
-    const newCategories = await createCategories(categories);
+        // 1 - Se já tem TMDB ID, busca direto
+        if (stream.tmdb_id) {
+            const { data } = await axios.get(
+                `https://api.themoviedb.org/3/movie/${stream.tmdb_id}`,
+                { params: { api_key: TMDB_API_KEY, language: TMDB_LANGUAGE } }
+            );
+            tmdbData = data;
+        }
+
+        // 2 - Se não encontrou pelo ID, busca por nome + ano
+        if (!tmdbData) {
+            const query = stream.stream_display_name;
+            const { data } = await axios.get(
+                `https://api.themoviedb.org/3/search/movie`,
+                {
+                params: {
+                    api_key: TMDB_API_KEY,
+                    query: cleanName(query),
+                    year: stream.year || undefined,
+                    language: TMDB_LANGUAGE,
+                },
+                }
+            );
+
+            if (data.results && data.results.length > 0) {
+                tmdbData = data.results[0];
+            }
+        }
+
+        if (!tmdbData) {
+            console.warn(
+                `⚠️ Não foi possível encontrar dados para: ${stream.stream_display_name} (${stream.tmdb_id || "sem id"})`
+            );
+        }
+        const existingCategoryNames = (JSON.parse(stream.category_id || "[]"))
+            .map((catId) => {
+                const cat = existingCategories.find((c) => c.id === catId);
+                return cat ? cat.category_name : null;
+            })
+            .filter(Boolean)
+            .join(", ");
+        
+        return {
+            ...{category_names: existingCategoryNames},
+            ...stream,
+            tmdb: tmdbData,
+        };
+    } catch (err) {
+        if(err?.message?.toString().includes('401')) {
+            console.error("❌ ============================================================= ❌ ");
+            console.error("Erro de autenticação com a TMDB. Verifique sua chave API.");
+            console.error("❌ ============================================================= ❌ ");
+            process.exit(1);
+        }
+        console.error(
+            `Erro TMDB: ${stream.stream_display_name} (${stream.tmdb_id || "sem id"})`,
+            err.message
+        );
+
+        return {
+            ...stream,
+            tmdb: null,
+        };
+    }
+}
+export async function organizeMovies(streams, categories, createCategories = true) {
+    if(createCategories) {
+        categories = await createCategories(categories);
+    }
 
     for (const stream of streams) {
         const category_ids = [];
         if(isAdult(stream)){
-            category_ids.push(newCategories.find(cat => cat.type === 'adult')?.id);
+            category_ids.push(categories.find(cat => cat.type === 'adult')?.id);
             await updateCategories(stream, category_ids);
             continue; // Se for adulto, não adiciona mais categorias
         }
         if(isSubtitled(stream)) {
-            category_ids.push(newCategories.find(cat => cat.type === 'subtitled')?.id);
+            category_ids.push(categories.find(cat => cat.type === 'subtitled')?.id);
         }
-        category_ids.push(...getGenreIds(stream, newCategories));
-        category_ids.push(...getYears(stream, newCategories));
+        category_ids.push(...getGenreIds(stream, categories));
+        category_ids.push(...getYears(stream, categories));
 
         // Se não preencher os requisitos, adiciona a categoria padrão
         if(category_ids.length === 0) {
-            category_ids.push(newCategories.find(cat => cat.type === 'uncategorized')?.id);
+            category_ids.push(categories.find(cat => cat.type === 'uncategorized')?.id);
         }
         await updateCategories(stream, category_ids);
 
@@ -193,11 +189,30 @@ async function createCategories(allCategories) {
             VALUES (?, 'movie', ?, ?)`,
             [cat.name, i, cat.is_adult || false]
         );
-        console.log('inserted', res.insertId);
         cat.id = res.insertId;
-        console.log(cat);
     }
     return allCategories;
+}
+
+/**
+ * Função para se utilizar quando todas as categorias ja contenham no banco de dados
+ * ela só compara os nomes e atualiza os IDs das categorias
+ * @param {*} allCategories 
+ */
+async function mapCategories(categories) {
+    const existinCategories = await db.query(
+        `SELECT id, category_name FROM streams_categories WHERE category_type = 'movie'`);
+    
+    for (const cat of categories) {
+        const existingCat = existinCategories[0].find(c => c.category_name === cat.name);
+        if (existingCat) {
+            cat.id = existingCat.id;
+        } else {
+            console.warn(`Categoria não encontrada: ${cat.name}`);
+            cat.id = null; // Ou trate de outra forma se necessário
+        }
+    }
+    return categories.filter(cat => cat.id !== null); // Filtra categorias sem ID
 }
 async function updateCategories(stream, categories) {
     await db.query(
@@ -206,4 +221,45 @@ async function updateCategories(stream, categories) {
     );
     console.log(`Categorias atualizadas para: ${stream.stream_display_name} => [${categories.join(', ')}]`);
 }
-createTMDB();
+
+/**
+ * Apenas as utilize, caso as categorias já estejam mapeadas e criadas
+ * @param {*} streams Envie apenas o registro do banco de dados do XUi
+ */
+
+/**
+    Exemplo de stream que deve ser enviado para a função categorizeMovies
+  [{
+    "id": 12868,
+    "category_id": "",
+    "stream_display_name": "O nome do seu filme",
+    "movie_properties": "{\"kinopoisk_url\":\"\",\"tmdb_id\":\"\",\"name\":\"XXX A Darker Shade\",\"o_name\":\"XXX A Darker Shade\",\"cover_big\":\"\",\"movie_image\":\"\",\"release_date\":\"\",\"episode_run_time\":\"\",\"youtube_trailer\":\"\",\"director\":\"\",\"actors\":\"\",\"cast\":\"\",\"description\":\"\",\"plot\":\"\",\"age\":\"\",\"mpaa_rating\":\"\",\"rating_count_kinopoisk\":0,\"country\":\"\",\"genre\":\"\",\"backdrop_path\":[\"\"],\"duration_secs\":5520,\"duration\":\"01:32:00\",\"video\":[],\"audio\":[],\"bitrate\":0,\"rating\":\"0\"}",
+    "year": null,
+    "tmdb_id": null
+  },]
+ */
+export async function categorizeMovies(streams, categories) {
+    if(!categories || categories.length === 0) { 
+        categories = JSON.parse(fs.readFileSync('./cat-movies.json', 'utf-8'));
+    }
+        
+    const mappedCategories = await mapCategories(categories);
+    const allResults = [];
+
+    // Processa em chunks
+    for (let i = 0; i < streams.length; i += CHUNK_SIZE) {
+        const chunk = streams.slice(i, i + CHUNK_SIZE);
+        console.log(`Processando chunk ${Math.floor(i / CHUNK_SIZE) + 1} (${chunk.length} filmes)`);
+
+        const processedChunk = await Promise.all(
+            chunk.map((stream) => limit(() => fetchFromTMDB(stream, mappedCategories)))
+        );
+
+        allResults.push(...processedChunk);
+        console.log(`✅ Chunk processado. Total acumulado: ${allResults.length}`);
+    }
+
+    await organizeMovies(allResults, mappedCategories, false);
+    console.log(`🎉 Organização de filmes concluída!`);
+    process.exit(1);
+}
